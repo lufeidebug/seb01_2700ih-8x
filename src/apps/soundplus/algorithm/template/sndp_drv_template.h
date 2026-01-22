f7f4eff77e7086963aed0dccb21b743b28b56411
linzhiwei <lzw@soundiot.cn>
2026-01-22 18:43:24 +0800

    修改算法模块Makefile

---
+++ src/apps/soundplus/algorithm/template/sndp_drv_template.h
@@ -0,0 +1,23 @@
+
+#ifndef __SPDRV_TEMPLATE_H__
+#define __SPDRV_TEMPLATE_H__
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+const sphal_temperature_s sphal_temperature_template;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  //__SPDRV_TEMPLATE_H__
+
+
+
+
+
