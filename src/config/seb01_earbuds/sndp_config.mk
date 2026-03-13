07f99724f44d4770a82eceeaed4c3ec9772560b4
linzhiwei <lzw@soundiot.cn>
2026-03-13 09:34:50 +0800

    打开左右耳同步通道 export SNDP_COMM_MS := 1

--- src/config/seb01_earbuds/sndp_config.mk
+++ src/config/seb01_earbuds/sndp_config.mk
@@ -240,7 +240,7 @@ ifeq ($(SNDP_COMMUNICATION_MGR),1)
     export SNDP_COMM_POGOPIN                    := 1
     export SNDP_COMM_BLE                        := 1
     export SNDP_COMM_SPP                        := 0
-    export SNDP_COMM_MS                         := 0
+    export SNDP_COMM_MS                         := 1
 endif
 
 ifeq ($(SNDP_COMM_TRACE_UART),1)
