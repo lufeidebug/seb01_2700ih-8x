458a20c26531e835900d3e1a7fc66982e13598f9
linzhiwei <lzw@soundiot.cn>
2026-03-19 21:20:13 +0800

    重新配置ANC MIC通道，FF=CH1, FB=CH0

--- src/config/seb01_earbuds/tgt_hardware.h
+++ src/config/seb01_earbuds/tgt_hardware.h
@@ -195,9 +195,9 @@ extern const struct HAL_IOMUX_PIN_FUNCTION_MAP cfg_hw_pinmux_pwl[CFG_HW_PWL_NUM]
 #define ANC_COEF_LIST_NUM                   (2)
 #endif
 
-#define ANC_FF_MIC_CH_L                     AUD_CHANNEL_MAP_CH0
+#define ANC_FF_MIC_CH_L                     AUD_CHANNEL_MAP_CH1
 #define ANC_FF_MIC_CH_R                     0
-#define ANC_FB_MIC_CH_L                     AUD_CHANNEL_MAP_CH1
+#define ANC_FB_MIC_CH_L                     AUD_CHANNEL_MAP_CH0
 #define ANC_FB_MIC_CH_R                     0
 
 /**
