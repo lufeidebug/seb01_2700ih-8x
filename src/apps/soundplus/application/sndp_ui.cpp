c53608c39a2ae3415f378800b792bba4b8b18912
linzhiwei <lzw@soundiot.cn>
2026-03-25 13:52:15 +0800

    打开无手机记录，开机进入配对模式

--- src/apps/soundplus/application/sndp_ui.cpp
+++ src/apps/soundplus/application/sndp_ui.cpp
@@ -1262,7 +1262,7 @@ static void sndp_ui_bt_event_exec_after_power_on(void)
                 SPUI_TRACE(0, "no paired record, tws pairing");
                 
                 //sndp_delay_exec_start(300, (uint32_t)media_PlayAudio, AUD_ID_BT_PAIRING, 0, 0);
-                //sndp_enter_mobile_pairing_after_tws_connected();
+                sndp_enter_mobile_pairing_after_tws_connected();
             }
         } else {
             SPUI_TRACE(0, "the left and right not bound.");
