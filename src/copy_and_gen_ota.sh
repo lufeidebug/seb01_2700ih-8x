ba72a4d22af30d9b614d5f3dadc56b703a9a40cc
linzhiwei <lzw@soundiot.cn>
2026-04-24 16:58:26 +0800

    修改 copy_and_gen_ota.sh。删除之前的bin文件。

--- src/copy_and_gen_ota.sh
+++ src/copy_and_gen_ota.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-proj_name=seb01_earbuds
+echo
+echo
 
+proj_name=seb01_earbuds
+echo proj_name=${proj_name}
 
 
 #获取当前日期字符串
@@ -13,13 +16,13 @@ echo dt_str=$dt_str
 
 #获取固件版本号
 fw_ver_file_path=config/${proj_name}/sndp_config.mk
-echo cfg_mk_path=${cfg_mk_path}
+#echo cfg_mk_path=${fw_ver_file_path}
 
 sw_ver_str=$(grep "SOFTWARE_VERSION" ${fw_ver_file_path})
-echo ${sw_ver_str}
+#echo ${sw_ver_str}
 
 sw_ver=${sw_ver_str##*=} 
-echo ${sw_ver}
+#echo ${sw_ver}
 
 trim_sw=$(echo ${sw_ver} | sed -e 's/^[ ]*//g' | sed -e 's/[ ]*$//g')
 
@@ -27,8 +30,11 @@ trim_sw=$(echo ${sw_ver} | sed -e 's/^[ ]*//g' | sed -e 's/[ ]*$//g')
 #trim_sw="{trim_sw#"{trim_sw%%[![:space:]]*}"}"   # 去掉前导空格
 #trim_sw="{trim_sw%"{trim_sw##*[![:space:]]}"}"  # 去掉尾部空格
 
-echo trim_sw=${trim_sw}
+#echo trim_sw=${trim_sw}
 
+
+#删除之前的bin
+rm ${proj_name}_app_*.bin
 
 
 #bin文件源目录
@@ -36,9 +42,11 @@ app_bin_src_path=./out/${proj_name}/${proj_name}.bin
 #echo app_bin_src=$app_bin_src
 
 #bin文件拷贝到的目录
-app_bin_sW=${proj_name}_app_v${trim_sw}.bin
-app_bin_ota=${proj_name}_app_for_ota_v${trim_sw}.bin
+app_bin_ver=${proj_name}_app_v${trim_sw}.bin
 app_bin_dt=${proj_name}_app_${dt_str}.bin
+
+ota_bin_ver=${proj_name}_app_for_ota_v${trim_sw}.bin
+ota_bin_dt=${proj_name}_app_for_ota_${dt_str}.bin
 
 #echo bin_des=$bin_des
 
@@ -47,15 +55,22 @@ if [ -e $app_bin_src_path ]; then
 #    echo "文件存在"
 	
 	#拷贝bin文件
-	echo cp -f ${app_bin_src_path} ./${app_bin_sW}
-	cp -f ${app_bin_src_path} ./${app_bin_sW}
+	echo cp -f ${app_bin_src_path} ./${app_bin_ver}
+	cp -f ${app_bin_src_path} ./${app_bin_ver}
+	
+	echo cp -f ${app_bin_src_path} ./${app_bin_dt}
 	cp -f ${app_bin_src_path} ./${app_bin_dt}
 	
-	./tools/build_compressed_ota.sh ./${app_bin_sW} ./${app_bin_ota}
+	echo build_compressed_ota.sh ./${app_bin_ver} ./${ota_bin_ver}
+	./tools/build_compressed_ota.sh ./${app_bin_ver} ./${ota_bin_ver}
+	
+	echo cp -f ${ota_bin_ver} ./${ota_bin_dt}
+	cp -f ${ota_bin_ver} ./${ota_bin_dt}
 	
 	rm crc.bin
 	rm magic.bin
 	
+	echo
 else
     echo "文件不存在"
 fi
