0077fa18135a818ec25ba186f51d232a7271a9c7
lanjie <lanjie@soundiot.cn>
2026-04-24 18:31:44 +0800

    修改版本为V0.0.0.13

--- src/config/seb01_earbuds/sndp_config.mk
+++ src/config/seb01_earbuds/sndp_config.mk
@@ -5,7 +5,7 @@ ifeq ($(SNDP_PROJ),1)
 KBUILD_CPPFLAGS += -D__SNDP_PROJ__
 
 export HARDWARE_VERSION ?= 1.0
-export SOFTWARE_VERSION ?= 0.0.0.12
+export SOFTWARE_VERSION ?= 0.0.0.13
 
 # platform macro configuration
 export TRACE_BAUD_RATE 							:= 1152000
