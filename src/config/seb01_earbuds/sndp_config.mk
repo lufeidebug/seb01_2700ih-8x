98711ccf997cca9c08a9e71f2cd12dcf5aeeef89
linzhiwei <lzw@soundiot.cn>
2026-04-28 13:42:15 +0800

    V0.0.0.15

--- src/config/seb01_earbuds/sndp_config.mk
+++ src/config/seb01_earbuds/sndp_config.mk
@@ -5,7 +5,7 @@ ifeq ($(SNDP_PROJ),1)
 KBUILD_CPPFLAGS += -D__SNDP_PROJ__
 
 export HARDWARE_VERSION ?= 1.0
-export SOFTWARE_VERSION ?= 0.0.0.14
+export SOFTWARE_VERSION ?= 0.0.0.15
 
 # platform macro configuration
 export TRACE_BAUD_RATE 							:= 1152000
