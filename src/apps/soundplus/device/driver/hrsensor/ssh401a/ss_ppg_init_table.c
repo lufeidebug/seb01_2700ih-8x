0cf5ee167cf298dba3107c5c23e3b9443b8f8781
linzhiwei <lzw@soundiot.cn>
2026-05-11 15:22:05 +0800

    更新SSH401A初始化参数。

--- src/apps/soundplus/device/driver/hrsensor/ssh401a/ss_ppg_init_table.c
+++ src/apps/soundplus/device/driver/hrsensor/ssh401a/ss_ppg_init_table.c
@@ -35,8 +35,8 @@ const unsigned char g_ss_ppg_init_register_ssh202[][2] = {
     // Hidden options
     {REG_HIDDEN_KEY, 0x0A},      // Test key enable
     {REG_H_AFE_TIME, 0xE1},      // AFE Rest time control
-    {REG_SEQ0_H_TIA_GAIN, 0x33},      // TIA Gain=50Kohm (x8)
-    {REG_SEQ0_H_INT_GAIN, 0x44},      // INT Gain=100Kohm/8pF
+    {REG_H_TIA_GAIN, 0x33},      // TIA Gain=50Kohm (x8)
+    {REG_H_INT_GAIN, 0x44},      // INT Gain=100Kohm/8pF
 };
 
 /**
@@ -45,9 +45,8 @@ const unsigned char g_ss_ppg_init_register_ssh202[][2] = {
  * This array is the initialization table for the SSH401.\n
  * It may need to be optimized for the actual application.
  */
-
- /**
 const unsigned char g_ss_ppg_init_register_ssh401[][2] = {
+#if 0
     {REG_SEQ_LED_SEL, 0x3F},     // LED ALL OFF
     {REG_MEASUREMENT, 0x08},     // MEASURE ON
     {REG_MEASUREMENT, 0x00},     // MEASURE OFF
@@ -70,79 +69,71 @@ const unsigned char g_ss_ppg_init_register_ssh401[][2] = {
     {REG_H_AFE_TIME, 0xE1},      // AFE Rest time control
     {REG_H_TIA_GAIN, 0x33},      // TIA Gain=50Kohm (x8)
     {REG_H_INT_GAIN, 0x44},      // INT Gain=100Kohm/8pF
-};
-*/
+#else
 
-//SOLUM test version (260423)
-const unsigned char g_ss_ppg_init_register_ssh401[][2] = {
-    
-    // Gen2 initial configuration for Proximity mode and PPG mode.
-    // Sensor operating modes and behavior are controlled through the REG_MEASUREMENT register.
-    
-    //[1]FIFO Initialization
+   // NOTE:
+   // This function is configured for PPG Green LED operation.
+   // For PPG IR operation, register values must be reconfigured
+   // (LED current, timing, and Gain settings).
+
+     // FIFO initializatio
     {REG_SEQ_LED_SEL, 0x3F},     // LED ALL OFF
     {REG_MEASUREMENT, 0x08},     // MEASURE ON
     {REG_MEASUREMENT, 0x00},     // MEASURE OFF
     {REG_FIFO_FLUSH, 0x01},      // CLEAR FIFO
 
-    //[2]Hidden Key enable
-    {REG_HIDDEN_KEY, 0x0A},      // Test key enable
-    
-    //[3]LED and PD Settings Register
-    {REG_SEQ_LED_SEL, 0x1B},     // SEQ0=VCSEL(prox_source), SEQ1=Green, SEQ2=Disabled
-    {REG_SEQ_PD_SEL, 0x85},      // SEQ0=Internal PD, SEQ1= External PD SEQ2=GND
+    // LED & PD configurati
+    {REG_SEQ_PD_SEL, 0x85},      // SEQ0=Internal PD, SEQ1,2=External PD
+    {REG_SEQ_LED_SEL, 0x1B},     // SEQ0=VCSEL, SEQ1=Green,SEQ2=Disabled
 
-    //[4]Set LED Current(mA) and Integration Time (=LED On Time)
-    {REG_SEQ0_LED_CUR, 0x2C},    // LED Current=15.26mA
-    {REG_SEQ0_LED_RANGE, 0x02},  // LED Current Range setting (43.4mA)
+    // LED drive current configuration
+    {REG_SEQ0_LED_RANGE, 0x01},  // LED Current Range setting
+    {REG_SEQ0_LED_CUR, 0x2C},    // LED Current=10.58mA
+    {REG_SEQ1_LED_RANGE, 0x01},  // LED Current Range setting
+    {REG_SEQ1_LED_CUR, 0x2E},    // LED Current=11.05mA
 
-    {REG_SEQ1_LED_CUR  , 0x2E},    // LED Current=11.05mA
-    {REG_SEQ1_LED_RANGE , 0x01},  // LED Current Range setting (30.1mA)
+    // LED on-time configuration
+    {REG_SEQ0_INTG_TIME_H, 0x00},// Integration time high byte (=(N+1)*0.5us = 38us)
+    {REG_SEQ0_INTG_TIME_L, 0x4B},// Integration time low byte (=(N+1)*0.5us = 38us)
+    {REG_SEQ1_INTG_TIME_H, 0x00},// Integration time high byte (=(N+1)*0.5us = 40us)
+    {REG_SEQ1_INTG_TIME_L, 0x4F},// Integration time low byte (=(N+1)*0.5us = 40us)
 
-    {REG_SEQ0_INTG_TIME_H, 0x00},// Integration time (=(N+1)*0.5us = 38us)
-    {REG_SEQ0_INTG_TIME_L, 0x4B},
-
-    {REG_SEQ1_INTG_TIME_H, 0x00},// Integration time (=(N+1)*0.5us = 40us)
-    {REG_SEQ1_INTG_TIME_L, 0x4F},
-
-    //[5]Set DAC Range
-    {REG_SEQ0_DAC_RANGE, 0x02},  // ADAC=6.38uA, DDAC=220uA
-    {REG_SEQ1_DAC_RANGE, 0x32},  // ADAC=25.5uA, DDAC=220uA
-    {REG_DDAC_H_LIN_CAL, 0x02},  // DDAC linearity correction register  
-
-    //[6]Hidden Register Settings
-    {REG_H_AFE_TIME, 0xE0},       // AFE Rest time control
-    {REG_SEQ0_H_TIA_GAIN, 0x56}, // TIA Gain=50Kohm (x8)
-    {REG_SEQ0_H_INT_GAIN, 0x35}, // INT Gain=100Kohm/8pF
-
-    {REG_SEQ1_H_TIA_GAIN, 0x43}, // TIA Gain=50Kohm (x16)
-    {REG_SEQ1_H_INT_GAIN, 0x45}, // INT Gain=100Kohm/16pF
-
-    //[7] Interrupt Settings
-    {REG_INTREEUPT_CONF, 0x80},  // interrupt enable(A_FIFO_FULL)
-
-    //* Note)PPG stops when earbuds are removed from the ear during PPG measurement
-    //{REG_INTREEUPT_CONF, 0x82},  // interrupt enable(A_FIFO_FULL& Proximity)
-    
-    //{REG_INT_MODE , 0x11},      // Set interrupt after interrupt state is released
-    {REG_INT_MODE_SET , 0x01},   // Mode: Threshold mode , Persistence : 2
-
-    //[8]Proximity mode settings
-    {REG_PROX_GAIN, 0x0A},          // Proximity Gain : x2
-    {REG_PROX_AVG_FLT, 0x00},       // Proximity Moving Average Filter : Disable 
-    {REG_PROX_THRES_HIGH_H, 0x9C},  // Threshold : 40000
-    {REG_PROX_THRES_HIGH_L, 0x40},  // 
-    {REG_PROX_THRES_LOW_H, 0x4E},   ///Threshold : 20000
-    {REG_PROX_THRES_LOW_L, 0x20},   //
-
-    //[9]Other recommended settings
+    // Other sensor setting
+    {REG_PPG_TH_SEL, 0x00},      // PPG Mode DAC threshold value
     {REG_PPG_FILTER, 0x00},      // PPG Filter Disable
     {REG_DC_CUT_MODE, 0x80},     // DC cut
+
+    // Hidden options
+    {REG_HIDDEN_KEY, 0x0A},      // Test key enable
+
+    // DAC Range setting
+    {REG_SEQ0_DAC_RANGE, 0x02},  // ADAC=6.38uA, DDAC=220uA
+    {REG_SEQ1_DAC_RANGE, 0x32},  // ADAC=25.5uA, DDAC=220uA
+    {REG_DDAC_CO_FI_SEL, 0x02},  // ADAC=25.5uA, DDAC=220uA
+
+    // Hidden register settings
+    {REG_H_TIA_GAIN, 0x63},      // SEQ0 TIA Gain=400Kohm (x16)
+    {REG_H_INT_GAIN, 0x35},      // SEQ0 INT Gain=50Kohm/16pF
+    {REG_SEQ1_H_TIA_GAIN, 0x43},  // SEQ1 TIA Gain=100Kohm (x16)
+    {REG_SEQ1_H_INT_GAIN, 0x45}, // SEQ1 INT Gain=100Kohm/16pF
+
+    {REG_H_AFE_TIME, 0xE0},      // AFE Rest time control
+
+    // Interrupt settings
+    {REG_INTREEUPT_CONF, 0x82},  // A_FIFO_FULL and PROX_INT_EN interrupt enable
+    {REG_PROX_STATE_MODE, 0x01}, // interrupt mode : threshold mode , Persistence : 2
+    {REG_INT_MODE, 0x01},        // Interrupt clear mode , Bitp[4]=0 : Interrupt reoccurs only on a new event after clear
+
+    // Proximity settings
+    {REG_PROX_DIGITAL_GAIN, 0x0A}, // Proximity Gain = x2 
+    {REG_PROX_FILTER_SET, 0x00},   // Moving average filter : Disable
 
     //[10]Sensor sequence mode configuration
     {REG_MEASUREMENT, 0x60},     // MODE setting = SEQ0:Proximity mode , SEQ1: PPG mode
     //Enable Proximity Mode (SEQ0) only start : Write 0x08 to REG_MEASUREMENT
     //Enable PPG Mode (SEQ1) only start : Write 0x38 to REG_MEASUREMENT
+#endif
+	
 };
 
 const unsigned char ss_ppg_init_register_seq1_ir_ssh401[][2] = {
--- src/apps/soundplus/device/driver/hrsensor/ssh401a/ss_ppg_register.h
+++ src/apps/soundplus/device/driver/hrsensor/ssh401a/ss_ppg_register.h
@@ -359,14 +359,23 @@ extern "C" {
 /** @cond INTERNAL */
 #define REG_HIDDEN_KEY          0xFF
 #define REG_H_AFE_TIME          0x37
-#define REG_SEQ0_H_TIA_GAIN    0x86
-#define REG_SEQ0_H_INT_GAIN    0x87
-#define REG_SEQ1_H_TIA_GAIN    0x88
-#define REG_SEQ1_H_INT_GAIN    0x89
-#define REG_DDAC_H_LIN_CAL     0xA3
-#define REG_INT_MODE_SET       0x03
-#define REG_PROX_GAIN          0xB6
-#define REG_PROX_AVG_FLT       0x2F
+#define REG_SEQ0_H_TIA_GAIN     0x86
+#define REG_SEQ0_H_INT_GAIN     0x87
+#define REG_SEQ1_H_TIA_GAIN     0x88
+#define REG_SEQ1_H_INT_GAIN     0x89
+#define REG_DDAC_H_LIN_CAL      0xA3
+#define REG_INT_MODE_SET        0x03
+#define REG_PROX_GAIN           0xB6
+#define REG_PROX_AVG_FLT        0x2F
+
+#define REG_H_TIA_GAIN          0x86
+#define REG_H_INT_GAIN          0x87
+#define REG_PPG_TH_SEL          0xB1
+#define REG_DDAC_CO_FI_SEL      0xA3
+#define REG_PROX_STATE_MODE     0x03
+#define REG_PROX_DIGITAL_GAIN   0xB6
+#define REG_PROX_FILTER_SET     0x2F        
+
 
 
 /** @endcond */
