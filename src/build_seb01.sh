57d99aee05d0c2e985b7bbb9c59f5fe452f8de30
linzhiwei <lzw@soundiot.cn>
2026-07-22 18:30:06 +0800

    修改编译脚本，在编译前删除 seb01_earbuds.bin ，这样在编译报错的时候，不会执行 copy_and_gen_ota.sh 部分操作。

--- src/build_seb01.sh
+++ src/build_seb01.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export PATH=/usr/bin/gcc-arm-none-eabi-10.3-2021.07/bin:$PATH
 
+rm ./out/seb01_earbuds/seb01_earbuds.bin
+
 #./tools/com_sdk/cust/build_pro_1503_0003_seb01.sh $1
 ./tools/com_sdk/cust/build_pro_1503_0001_seb01.sh $1
 
