47e8c81eacfc9e82a0b48a08d53328fd25d16dc9
kimi <1210675397@qq.com>
2026-09-08 11:03:35 +0800

    V0.0.0.32

--- src/config/seb01_earbuds/sndp_config.mk
+++ src/config/seb01_earbuds/sndp_config.mk
@@ -5,7 +5,7 @@ ifeq ($(SNDP_PROJ),1)
 KBUILD_CPPFLAGS += -D__SNDP_PROJ__
 
 export HARDWARE_VERSION ?= 1.0
-export SOFTWARE_VERSION ?= 0.0.0.31
+export SOFTWARE_VERSION ?= 0.0.0.32
 
 # platform macro configuration
 export TRACE_BAUD_RATE 							:= 1152000
